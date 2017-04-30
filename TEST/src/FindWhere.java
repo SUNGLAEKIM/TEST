4ae8eae8f384557847a57c2f2a4e47f26f185ed1
speed <speed@DESKTOP-MSJ10UT>
2017-04-30 11:04:26 +0900

    main, FindWhere

--- TEST/src/FindWhere.java
+++ TEST/src/FindWhere.java
@@ -1,9 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FindWhere {
 
 	ArrayList<DataSet> data;
 	ArrayList<String> result=new ArrayList<String>();
+	private String key="";
+	HashMap<String, String> dataMap;
+	private int lineSize=0;
+	
 	public FindWhere(ArrayList<DataSet> data){
 		this.data=data;
 	}
@@ -11,7 +16,51 @@ public class FindWhere {
 	
 	
 	public ArrayList<String> result(String key ,String value){
-	
+		this.key=key+"";
+			
+		//System.out.println(dataMap.get("16"));
+		for(int i=0;i<data.size();i++){//file 갯수 만큼
+			
+			dataMap=data.get(i).getHashMap();
+			
+			//System.out.println(this.key);
+			lineSize=dataMap.size()/6;
+			//System.out.println(lineSize);
+			//System.out.println(dataMap.get("16"));
+			for(int k=1;k<=lineSize;k++){
+				this.key=k+key+"";
+				
+				//System.out.println(value+" : "+dataMap.get(this.key));
+				//System.out.println(this.key+" : "+dataMap.get(this.key));
+				if((dataMap.get(this.key).contains(value))){
+					System.out.println((data.get(i).fileName+" 의 ["+k+"] 번째 줄 : "
+							+dataMap.get(k+"1")+" "
+							+dataMap.get(k+"2")+" "
+							+dataMap.get(k+"3")+" "
+							+dataMap.get(k+"4")+" "
+							+dataMap.get(k+"5")+" "
+							+dataMap.get(k+"6")+" "
+									));
+					result.add(
+							data.get(i).fileName+" 의 ["+k+"] 번째 줄 : "
+									+dataMap.get(k+"1")+" "
+									+dataMap.get(k+"2")+" "
+									+dataMap.get(k+"3")+" "
+									+dataMap.get(k+"4")+" "
+									+dataMap.get(k+"5")+" "
+									+dataMap.get(k+"6")+" "
+											
+							
+							);
+				}					
+						
+			}
+			
+			
+		}
+		//System.out.println(data.size());
+		//System.out.println(dataMap.size());
+		
 		
 		return result;
 	}
--- TEST/src/main.java
+++ TEST/src/main.java
@@ -5,9 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		FileWrite fw=new FileWrite();
-		fw.write();	
+		//fw.write();	
 		
-		System.out.println(System.getProperty("user.home"));
+		//System.out.println(System.getProperty("user.home"));
 		
 		FileRead fr=new FileRead();
 		
@@ -19,18 +19,18 @@ public class main {
 		for(int k = 0; k < data.size(); k++) {
 	          System.out.println("k : " + k);
 	          for(int l = 0; l < data.get(k).dataMap.size()/6; l++) {
-	             System.out.print(l+1 + ": ");
+	            // System.out.print(l+1 + ": ");
 	             for(int m = 0; m < 6; m++) {
 	                String key = String.valueOf(l+1)+String.valueOf(m+1);
-	                System.out.print(data.get(k).dataMap.get(key) + " / ");
+	                //System.out.print(data.get(k).dataMap.get(key) + " / ");
 	             }
-	             System.out.println();
+	            // System.out.println();
 	          }   
 	       }
 		
 		
 		FindWhere where =new FindWhere(data);
-		
+		where.result("6", "VSTH832719");
 		//key, value 받는 로직
 		//ArrayList<String> result=where.result(key, value);
 		// result 를 파일에 출력
